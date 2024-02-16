494a89e6e00e4d7fada6b50448869eecc8afa7ad
Aayush Rijal <arijal@aiims.com.au>
2024-02-16 16:41:22 +1100

    SMTP installed

--- app/src/form.php
+++ app/src/form.php
@@ -1,6 +1,14 @@
 <?php
 include __DIR__ . '/../functions.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../vendor/phpmailer/phpmailer/src/SMTP.php';
+require '../vendor/autoload.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token']) && $_POST['formType'] === 'banner_form') {
     $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
     $recaptcha_secret = $recaptcha_server_secret;
@@ -64,19 +72,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token']) && $_POST['f
             '</tr>' .
             '</tbody></table></body></html>';
 
-        $headers = "MIME-Version: 1.0\r\n" .
-            "Content-type: text/html; charset=utf-8\r\n" .
-            "From: " . $site . " <" . $no_reply_email . ">" . "\r\n" .
-            // "Bcc: " . $bcc_email . "\r\n" .
-            "Reply-To: " . $site . " <" . $email . ">" . "\r\n" .
-            "X-Mailer: PHP/" . phpversion();
-        $result = mail($to, $subject, $message, $headers);
+        _phpmailer($to, $site, $subject, $message, $no_reply_email, $cc_email, $bcc_email);
 
-        if ($result) {
-            header('location:./../thankyou');
-        } else {
-            throw new Exception('Failed, please submit form again or call us directly.');
-        }
+        header('location:./../thankyou.php');
     } catch (Exception $e) {
         echo '<script language="javascript">alert("' . $e->getMessage() . '")</script>';
         echo '<script language="javascript">history.go(-1);</script>';
@@ -150,21 +148,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['token']) && $_POST['f
             '</tr>' .
             '</tbody></table></body></html>';
 
-        $headers = "MIME-Version: 1.0\r\n" .
-            "Content-type: text/html; charset=utf-8\r\n" .
-            "From: " . $site . " <" . $no_reply_email . ">" . "\r\n" .
-            // "Bcc: " . $bcc_email . "\r\n" .
-            "Reply-To: " . $site . " <" . $email . ">" . "\r\n" .
-            "X-Mailer: PHP/" . phpversion();
-        $result = mail($to, $subject, $message, $headers);
+        _phpmailer($to, $site, $subject, $message, $no_reply_email, $cc_email, $bcc_email);
 
-        if ($result) {
-            header('location:./../thankyou');
-        } else {
-            throw new Exception('Failed, please submit form again or call us directly.');
-        }
+        header('location:./../thankyou.php');
     } catch (Exception $e) {
         echo '<script language="javascript">alert("' . $e->getMessage() . '")</script>';
         echo '<script language="javascript">history.go(-1);</script>';
+    }
+}
+
+
+function _phpmailer($to_email, $site, $subject, $message, $no_reply_email, $cc, $bcc)
+{
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = TRUE;
+        $mail->SMTPSecure = "ssl";
+        $mail->Port     = 465;
+        $mail->Username = "aayush.rijal99@gmail.com";
+        $mail->Password = "koqg rtwa oxqi vjon";
+        $mail->Host     = "smtp.gmail.com";
+        $mail->Mailer   = "smtp";
+        $mail->SetFrom($no_reply_email, $site);
+        $mail->AddAddress($to_email);
+        if ($cc != '') {
+            $mail->addCC($cc);
+        }
+        if ($bcc != '') {
+            $mail->addBCC($bcc);
+        }
+        $mail->Subject = $subject;
+        $mail->WordWrap   = 80;
+        $mail->MsgHTML($message);
+        $mail->IsHTML(true);
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
